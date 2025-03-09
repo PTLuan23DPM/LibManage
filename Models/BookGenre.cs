bf29e07b24cd49d2b1609088e547d6cad6b937b7
PTL <13174@student.vgu.edu.vn>
2025-03-09 19:57:33 +0700

    initial commit

---
+++ Models/BookGenre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using LibWeb.Models;
+public class BookGenre
+{
+    public string BookID { get; set; }
+    [ForeignKey("BookID")]
+    public Book Book { get; set; }
+
+    public string GenreID { get; set; }
+    [ForeignKey("GenreID")]
+    public Genre Genre { get; set; }
+}
---
+++ Models/Rating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LibWeb.Models;
+public class Rating
+{
+    [Key]
+    [MaxLength(20)]
+    public string RatingID { get; set; }
+
+    [Required]
+    public string UserID { get; set; }
+    [ForeignKey("UserID")]
+    public User User { get; set; }
+
+    [Required]
+    public string BookID { get; set; }
+    [ForeignKey("BookID")]
+    public Book Book { get; set; }
+
+    [Required]
+    [Range(1, 10)]
+    public int RatingValue { get; set; }
+}
