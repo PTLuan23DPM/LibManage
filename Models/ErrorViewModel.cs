d92f8a834f142897f2ba22f21e9b877d98488c68
PTLuan23DPM <phanthailuank16@siu.edu.vn>
2025-03-29 23:07:03 +0700

    Revert "initial commit"
    This reverts commit bf29e07b24cd49d2b1609088e547d6cad6b937b7.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LibWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
