1eecbe037f5f5561e5e45c15a0bd4316c49a0447
PTLuan23DPM <phanthailuank16@siu.edu.vn>
2025-03-29 23:22:14 +0700

    Relaunch

---
+++ LibWeb/LibWeb/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibWeb.Models
+{
+    public class Book
+    {
+        [Key]
+        [MaxLength(20)]
+        public string BookID { get; set; }
+
+        [MaxLength(20)]
+        public string ImgFile { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Author { get; set; }
+
+        [Required]
+        public int AvailableQuantity { get; set; }
+
+        public float AverageRating { get; set; } = 0;
+
+        public ICollection<Rating> Ratings { get; set; }
+        public ICollection<BorrowDetail> BorrowDetails { get; set; }
+        public List<BookGenre> BookGenres { get; set; } = new List<BookGenre>();
+    }
+}
---
+++ LibWeb/LibWeb/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class User
+{
+    [Key]
+    [MaxLength(20)]
+    public string UserID { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string PasswordHash { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    [EnumDataType(typeof(UserRole))]
+    public string Role { get; set; }
+
+    public ICollection<Rating> Ratings { get; set; }
+    public ICollection<Borrow> Borrows { get; set; }
+}
+public enum UserRole
+{
+    Admin,
+    Thuthu,
+    Nguoidung
+}
